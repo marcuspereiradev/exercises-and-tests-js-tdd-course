f22c422ddb95b2c5f25032ab3605b75a40afe7f7
marcuspereiragithub <vinnycoast@gmail.com>
2018-12-03 15:31:40 -0300

    JS-Modules

--- exercises-and-tests/js-modules/webpack.config.js
+++ exercises-and-tests/js-modules/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -10,18 +11,23 @@ module.exports = {
         filename: 'build.js',
         path: path.resolve(__dirname, 'dist')
     },
-    // module: {
-    //     rules: [
-    //       {
-    //         test: /\.m?js$/,
-    //         exclude: /(node_modules|bower_components)/,
-    //         use: {
-    //           loader: 'babel-loader',
-    //           options: {
-    //             presets: ['@babel/preset-env']
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   },
+    optimization: {
+        minimizer: [new UglifyJsPlugin({
+            test: /\.js(\?.*)?$/i
+          })]
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [path.resolve(__dirname, './node_modules/@babel/preset-env')]
+              }
+            }
+          }
+        ]
+    },
 }
